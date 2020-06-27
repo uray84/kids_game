4300602f925c8ce2621b2034ab64689058cbfae6
uray84 <59328315+uray84@users.noreply.github.com>
2020-06-27 20:37:54 +0900

    update variable names import GUI library - actual GUI to be worked on

--- クイ～ズクイズ.py
+++ クイ～ズクイズ.py
@@ -1,9 +1,10 @@
 # 算数ゲーム
 import random
 import time
+import PySimpleGUI as sg
 
 # 質問と答えの記入
-tashizan2 = [["2+9=", "11"], ["3+8=", "11"], ["3+9=", "12"], ["4+7=", "11"],
+addition2 = [["2+9=", "11"], ["3+8=", "11"], ["3+9=", "12"], ["4+7=", "11"],
              ["4+8=", "12"], ["4+9=", "13"], ["5+6=", "11"], ["5+7=", "12"],
              ["5+8=", "13"], ["5+9=", "14"], ["6+5=", "11"], ["6+6=", "12"],
              ["6+7=", "13"], ["6+8=", "14"], ["6+9=", "15"], ["7+4=", "11"],
@@ -13,7 +14,7 @@ tashizan2 = [["2+9=", "11"], ["3+8=", "11"], ["3+9=", "12"], ["4+7=", "11"],
              ["9+2=", "11"], ["9+3=", "12"], ["9+4=", "13"], ["9+5=", "14"],
              ["9+6=", "15"], ["9+7=", "16"], ["9+8=", "17"], ["9+9=", "18"]]
 
-hikizan2 = [["11-2=", "9"], ["11-3=", "8"], ["11-4=", "7"], ["11-5=", "6"],
+subtraction2 = [["11-2=", "9"], ["11-3=", "8"], ["11-4=", "7"], ["11-5=", "6"],
             ["11-6=", "5"], ["11-7=", "4"], ["11-8=", "3"], ["11-9=", "2"],
             ["12-3=", "9"], ["12-4=", "8"], ["12-5=", "7"], ["12-6=", "6"],
             ["12-7=", "5"], ["12-8=", "4"], ["12-9=", "3"], ["13-4=", "9"],
@@ -26,41 +27,41 @@ hikizan2 = [["11-2=", "9"], ["11-3=", "8"], ["11-4=", "7"], ["11-5=", "6"],
 # 質問選択
 print("クイ～ズクイズ、なんのクイズ？")
 print("たしざん２： 1\nひきざん２： 2")
-quizchoice = input("クイズをえらんでください。 ")
-while quizchoice != "1" and quizchoice != "2":
-    quizchoice = input("もう１かい、クイズをえらんでください。 ")
+quiz_choice = input("クイズをえらんでください。 ")
+while quiz_choice != "1" and quiz_choice != "2":
+    quiz_choice = input("もう１かい、クイズをえらんでください。 ")
 
-if quizchoice == "1":
-    questionlist = tashizan2
+if quiz_choice == "1":
+    question_list = addition2
     print("たしざんのクイズやりましょう～")
 else:
-    questionlist = hikizan2
+    question_list = subtraction2
     print("ひきざんのクイズやりましょう～")
 
 # 順番をランダム可
-random.shuffle(questionlist)
+random.shuffle(question_list)
 
 start_time = time.time()
-mistakecount = 0
-tempmistake = 0
+mistake_count = 0
+temp_mistake = 0
 count = 0
 
 # 質問・答え確認
-for i in questionlist:
+for i in question_list:
     count += 1
-    print(str(count) + "/" + str(len(questionlist)) + ")")
+    print(str(count) + "/" + str(len(question_list)) + ")")
     question = (i[0])
     answer = (i[1])
-    quess = input(question)
-    while quess != answer:
+    guess = input(question)
+    while guess != answer:
         print("☓")
-        quess = input(question)
-        tempmistake = tempmistake + 1
+        guess = input(question)
+        temp_mistake = temp_mistake + 1
     else:
         print("◯\n")
-        if tempmistake >= 1:
-            mistakecount = mistakecount + 1
-            tempmistake = 0
+        if temp_mistake >= 1:
+            mistake_count = mistake_count + 1
+            temp_mistake = 0
 
 # 時間計算
 seconds = int(time.time() - start_time)
@@ -68,5 +69,5 @@ seconds = int(time.time() - start_time)
 # 結果発表
 print("おわりました、おめでとうございます！")
 print("かかったじかん：", int(seconds / 60), "ふん", seconds % 60, "びょう")
-print("まちがえたしつもん：", mistakecount, "けん")
+print("まちがえたしつもん：", mistake_count, "けん")
 print("明日もがんばりましょう～")
